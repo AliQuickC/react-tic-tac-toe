d28fcebd38ffa4fc620979f06b8e6277de8b17b2
Aleksandr <aleksandera9@gmail.com>
2023-01-19 17:26:19 +0300

    board cyclic layout

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class Board extends React.Component {
 	renderSquare(i) {
 		return (
 			<Square
+				key={i}
 				value={this.props.squares[i]}
 				onClick={() => this.props.onClick(i)}
 				victoryLine={this.props.victoryLine && this.props.victoryLine.includes(i)}
@@ -23,24 +24,30 @@ class Board extends React.Component {
 		);
 	}
 
+	renderRow = (rowNumb, countNumb) => {
+		const startNumb = rowNumb * 3
+		const squares = [];
+		for (let i = startNumb; i < startNumb + countNumb; i++) {
+			squares.push(this.renderSquare(i));
+		}
+		return 	<div key={rowNumb} className="board-row">
+							{squares}
+						</div>;
+	}
+
+	board = (size) => {
+		const boar = [];
+		for (let i = 0; i < size; i++) {
+			boar.push(this.renderRow(i, size));
+		}
+		return boar;
+	}
+
 	render() {
+
 		return (
 			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
+				{this.board(3)}
 			</div>
 		);
 	}
