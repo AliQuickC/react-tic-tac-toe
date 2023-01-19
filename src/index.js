43ea3e3860bf6555d1d4d42d739e8af86fc4efa8
Aleksandr <aleksandera9@gmail.com>
2023-01-19 05:02:49 +0300

    game state, history

--- src/index.js
+++ src/index.js
@@ -11,47 +11,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
-
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		if (calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			squares: squares,
-			xIsNext: !this.state.xIsNext,
-		});
-	}
-
 	renderSquare(i) {
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
 
 	render() {
-		const winner = calculateWinner(this.state.squares);
-		let status;
-		if (winner) {
-			status = 'Выиграл ' + winner;
-		} else {
-			status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
-		}
-
 		return (
 			<div>
-				<div className="status">{status}</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -73,15 +44,77 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [{ // массив с 1 элементом(массивом)
+				squares: Array(9).fill(null),
+			}],
+			stepNumber: 0, // номер хода
+			xIsNext: true, // первый ход за Х
+		};
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1); // новый массив состояний, от 0 до предыдущего хода
+		const current = history[history.length - 1];	// состояние предыдущего хода
+		const squares = current.squares.slice();			// копия массива, состояние предыдущего хода
+		if (calculateWinner(squares) || squares[i]) { // проверяем победу(предыдущий ход) или клетка не пустая
+			return;																			// блокируем дальнейшую обработку клика
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';// меняем элемент массива, получаем состояние текущиего хода
+		this.setState({// закидываем копию массива в state
+			history: history.concat([{ // созает новый массив, массив истории ходов +
+				squares: squares, // добавляем новый элемент, содержащий новый ход, в state
+			}]),
+			stepNumber: history.length, // устанавливаем текущий номер хода
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step) { // переход к ходу № по истории игры
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
+		});
+	}
+
 	render() {
+		const history = this.state.history; // массив ходов, (включая последний ход)
+		const current = history[this.state.stepNumber]; // массив, состояние последнего хода
+
+		const winner = calculateWinner(current.squares); // проверяем победу
+		// вывод инфо о ходе игры
+		let status;
+		if (winner) {
+			status = 'Выиграл ' + winner;
+		} else {
+			status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
+
+		// отрисовка кнопок с историей ходов игры
+		const moves = history.map((step, move) => {
+			const desc = move ? // текст кнопки
+				'Перейти к ходу №' + move :
+				'К началу игры'; // если индекс массива 0
+			return (
+				<li key={move}> {/* key - номер хода */}
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
 				</div>
 				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
+					<div>{status}</div>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		);
@@ -114,3 +147,5 @@ function calculateWinner(squares) {
 	}
 	return null;
 }
+
+// ========================================
